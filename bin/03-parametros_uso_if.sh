554f593e3462eaa403cbeec4a64ed05b0dd1d9ed
Hugo Aquino <hugo.aquino.navarrete@gmail.com>
2020-12-04 18:52:32 -0500

    Ajustes al script 03

--- bin/03-parametros_uso_if.sh
+++ bin/03-parametros_uso_if.sh
@@ -8,6 +8,10 @@ echo "Nombre del script:" $0
 if [ $# -eq 0 ]; then
   echo "Te faltó indicar el parámetro de entrada"
 
-elif [ $# -gt 0 ]; then
+elif [ $# -eq 1 ]; then
   echo "El valor del argumento que ingresaste fue:" $1
+
+elif [ $# -gt 1 ]; then
+  echo "Ingresaste $# argumentos"
+
 fi
