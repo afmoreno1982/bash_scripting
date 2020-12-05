f9020e3c833d1ee057ec6e11ebda93ea8ebee9b8
Hugo Aquino <hugo.aquino.navarrete@gmail.com>
2020-12-05 10:59:45 -0500

    Ajustes en redacción del reto 5

--- bin/reto_5.sh
+++ bin/reto_5.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Reto # 5. Toma como base el script “09-convierte_fahrenheit_centigrados.sh" para que “reto_5.sh” haga lo siguiente:
-#       1. Leer el archivo “libras.txt” que se encuentra en el directorio “input”.
-#       2. Crear una función que se llame “libras_a_kilos” que convierta el valor de libras a kilogramos, redondeando a 3 decimales.
-#       3. Crear un archivo en el directorio “output” que se llame “kilogramos.txt” con la conversión hecha.
+#       1. Lea el archivo “libras.txt” que se encuentra en el directorio “input”.
+#       2. Crea una función que se llame “libras_a_kilos” que convierta el valor de libras a kilogramos, redondeando a 3 decimales.
+#       3. Crea un archivo en el directorio “output” que se llame “peso.txt”, en la primer línea aparecerá “libras,kilogramos” y a partir de la segunda línea, el valor de libras seguido de una “,” y posteriormente el valor de la conversión a kilogramos (no debe hacer espacios entre los valores y la “coma”).
 
 
 
